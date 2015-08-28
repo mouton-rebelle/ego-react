171057669bff5733b59d357c658ab09bd2de2015
Cédric Fontaine <cf@eg0.me>
2015-08-28 14:33:27 +0200

    forgotten files

---
+++ containers/SinglePostPage.js
@@ -0,0 +1,54 @@
+import React, {PropTypes, Component} from 'react';
+import { connect } from 'react-redux';
+import { postLoadById } from '../actions/PostActions';
+import Post from '../components/Post';
+import PostDetail from '../components/PostDetail';
+
+
+
+@connect(state => ({
+  byId: state.posts.byId
+}))
+export default class SinglePostPage extends Component {
+
+  static propTypes={
+    byId: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    params: PropTypes.object
+  };
+
+  componentWillMount() {
+    console.log(this.props);
+    if (!this.props.byId[this.props.params.id])
+    {
+      this.props.dispatch(postLoadById(this.props.params.id));
+    }
+  }
+
+  render() {
+    const { byId, params } = this.props;
+    const p = byId[params.id];
+    if (params.imageId)
+    {
+      return (
+        p ? (<PostDetail
+          child={p.child}
+          desc={p.desc}
+          id={p._id}
+          imageId={params.imageId}
+          title={p.title}
+          />) : null
+      );
+    } else {
+      return (
+        p ? (<Post
+          child={p.child}
+          desc={p.desc}
+          horizontal={p.horizontal}
+          id={p._id}
+          title={p.title}
+          />) : null
+      );
+    }
+  }
+}
